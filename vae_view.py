8d9d2803f13745e8bc525e184290870c26667a67
Navid Korhani <n.korhani@gmail.com>
2020-04-06 17:28:24 -0400

    modularize code and add more metrics

--- vae_view.py
+++ vae_view.py
@@ -17,11 +17,10 @@ import os
 from torch.utils import data
 import random
 import argparse
+import json
 
 import wandb
 
-wandb_username = 'nkorhani'
-local_username = 'nkorhani'
 
 parser = argparse.ArgumentParser(description='VAE MNIST Example')
 parser.add_argument('--batch-size', type=int, default=128, metavar='N',
@@ -34,78 +33,24 @@ parser.add_argument('--seed', type=int, default=1, metavar='S',
                     help='random seed (default: 1)')
 parser.add_argument('--log-interval', type=int, default=10, metavar='N',
                     help='how many batches to wait before logging training status')
-
-parser.add_argument('--layers-dim', nargs='+', type=int)
-
+parser.add_argument('--layers-dim', nargs='+', type=int, default=[100])
 parser.add_argument('--input-dim', type=int, default=100, 
                     help='size of vae latent dimension')
 
 args = parser.parse_args()
-
-configs_dict = {'num_epochs':args.epochs,
-        'layers_dim':args.layers_dim, 'vae_latent_dim':args.input_dim}
-
-run_id = 'vae_view_'+" ".join(list(map(str, args.layers_dim)))
-
-wandb.init(project='hnultra', entity=wandb_username, name=run_id)
-wandb.config.update(configs_dict)
-
 print(args)
 
+configs_dict = {'num_epochs':args.epochs,'layers_dim':args.layers_dim, 'vae_latent_dim':args.input_dim}
 
-#raise Exception('my Error')
+run_id = 'vae_view_{}_input_dim_{}_layers_'.format(args.input_dim, len(args.layers_dim)) + "_".join(list(map(str, args.layers_dim)))
+print("run_id =",run_id)
+
+wandb_username = 'nkorhani'
+
 if torch.cuda.is_available():
     device = torch.device('cuda') 
 else:
     device = torch.device('cpu')
-
-# root directory
-andrea_dir = "/home/andreasabo/Documents/HNProject/"
-
-# data directory on current machine: abhishekmoturu, andreasabo, denizjafari, navidkorhani
-data_dir = "/home/navidkorhani/Documents/HNProject/HNUltra/latent100_images/"
-
-# read target df
-csv_path = os.path.join(andrea_dir, "all_splits_1000000.csv")
-data_df = pd.read_csv(csv_path, usecols=['subj_id', 'image_ids', 'view_label', 'view_train'])
-
-label_mapping = {'Other':0, 'Saggital_Right':1, 'Transverse_Right':2, 
-                 'Saggital_Left':3, 'Transverse_Left':4, 'Bladder':5}
-
-data_df['view_label'] = data_df['view_label'].map(label_mapping)
-
-train_df = data_df[data_df.view_train == 1]
-test_df = data_df[data_df.view_train == 0]
-
-labels = {}
-train_and_valid_subj_ids = []
-train_and_valid_image_ids = []
-test_ids = []
-
-for ind, row in train_df.iterrows():
-    train_and_valid_subj_ids.append(row['subj_id'])
-    train_and_valid_image_ids.append(row['image_ids'])
-    labels[row['image_ids']] = row['view_label']
-
-for ind, row in test_df.iterrows():
-    test_ids.append(row['image_ids'])
-    labels[row['image_ids']] = row['view_label']
-
-s = set()
-t_v_ids = pd.DataFrame(list(zip(train_and_valid_subj_ids, train_and_valid_image_ids)), columns=['subj_ids', 'image_ids'])
-id_groups = [t_v_ids for _, t_v_ids in t_v_ids.groupby('subj_ids')]
-random.shuffle(id_groups)
-id_groups = pd.concat(id_groups).reset_index(drop=True)
-train_val_split = int(0.8*len(set(id_groups['subj_ids'].values)))
-train_val_set = [i for i in id_groups['subj_ids'].values if not (i in s or s.add(i))]
-cutoff = train_val_set[train_val_split]
-train_portion = (id_groups['subj_ids'].values == cutoff).argmax()
-
-train_ids = id_groups[:train_portion]['image_ids'].tolist()
-valid_ids = id_groups[train_portion:]['image_ids'].tolist()
-
-partition = {'train':train_ids, 'valid':valid_ids, 'test':test_ids}
-
 
 
 class Dataset(data.Dataset):
@@ -131,21 +76,59 @@ class Dataset(data.Dataset):
 
         return z, y
 
+# root directory
+andrea_dir = "/home/andreasabo/Documents/HNProject/"
+
+data_dir = "/home/navidkorhani/Documents/HNProject/HNUltra/latent{}_images/".format(args.input_dim)
 
 # Parameters
 params = {'batch_size': args.batch_size,
           'shuffle': True,
           'num_workers': 6}
 
-# Datasets and Generators
-training_set = Dataset(partition['train'], labels)
-train_loader = DataLoader(training_set, **params)
+# read target df
+csv_path = os.path.join(andrea_dir, "all_splits_1000000.csv")
+data_df = pd.read_csv(csv_path, usecols=['subj_id', 'image_ids', 'view_label', 'view_train'])
 
-validation_set = Dataset(partition['valid'], labels)
-validation_loader = data.DataLoader(validation_set, **params)
+label_mapping = {'Other':0, 'Saggital_Right':1, 'Transverse_Right':2, 
+                 'Saggital_Left':3, 'Transverse_Left':4, 'Bladder':5}
 
-test_set = Dataset(partition['valid'], labels)
-test_loader = data.DataLoader(test_set, **params)
+reverse_mapping = {v:k for k,v in label_mapping.items()}
+
+data_df['view_label'] = data_df['view_label'].map(label_mapping)
+
+def get_test_loader():
+    test_df = data_df[data_df.view_train == 0]
+
+    test_ids = []
+    test_labels = {}
+    for ind, row in test_df.iterrows():
+        test_ids.append(row['image_ids'])
+        test_labels[row['image_ids']] = row['view_label']
+
+    test_dataset = Dataset(test_ids, test_labels)
+    test_loader = data.DataLoader(test_set, **params)
+
+    return test_loader
+        
+def get_train_and_val_loaders(fold):
+    with open('data_splits.json') as f:
+        splits = json.load(f)
+
+        train_ids = splits[fold]['train_ids']
+        train_labels = splits[fold]['train_labels']
+
+        val_ids = splits[fold]['valid_ids']
+        val_labels = splits[fold]['valid_labels']
+
+    # Datasets and Generators
+    train_dataset = Dataset(train_ids, train_labels)
+    train_loader = DataLoader(train_dataset, **params)
+
+    val_dataset = Dataset(val_ids, val_labels)
+    val_loader = data.DataLoader(val_dataset, **params)
+
+    return train_loader, val_loader
 
 class DeepClassifier(nn.Module):
     def __init__(self, input_dim=100, layers_dim = [200]):
@@ -154,68 +137,63 @@ class DeepClassifier(nn.Module):
         self.input_dim = input_dim
         self.layers_dim = layers_dim.copy()
 
-        self.fcns = []
-        current_dim = input_dim
-        for i in range(len(self.layers_dim)):
-            self.fcns.append(nn.Linear(current_dim, layers_dim[i]))
-            current_dim = layers_dim[i]
+        self.num_of_layers = len(self.layers_dim)
 
-        self.fcns.append(nn.Linear(current_dim, 6))
+        self.fc1 = nn.Linear(input_dim, self.layers_dim[0])
+
+        if self.num_of_layers == 2:
+            self.fc2 = nn.Linear(self.layers_dim[0], self.layers_dim[1])
+
+        if self.num_of_layers == 3:
+            self.fc3 = nn.Linear(self.layers_dim[1], self.layers_dim[2])
+
+        if self.num_of_layers == 4:
+            self.fc4 = nn.Linear(self.layers_dim[2], self.layers_dim[3])
+
+        if self.num_of_layers == 5:
+            self.fc5 = nn.Linear(self.layers_dim[3], self.layers_dim[4])
+
+
+        self.fc_last.append(nn.Linear(self.layers_dim[-1], 6))
         self.softmax = nn.Softmax(dim=1)
     
     
     def forward(self, x):
 
-        out = F.relu(self.fcns[0](x))
+        out = F.relu(self.fc1(x))
 
-        for i in range(1, len(self.layers_dim)):
-            out = F.relu(self.fcns[i](out))
+        if self.num_of_layers == 2:
+            out = F.relu(self.fc1(x))
 
-        out = self.softmax(self.fc3(out))
+        if self.num_of_layers == 3:
+            out = F.relu(self.fc2(x))
+
+        if self.num_of_layers == 4:
+            out = F.relu(self.fc3(x))
+
+        if self.num_of_layers == 5:
+            out = F.relu(self.fc4(x))
+
+        out = self.softmax(self.fc_last(out))
         
         return out
 
-classifier_model = DeepClassifier(input_dim=args.input_dim, layers_dim=args.layers_dim).to(device)
-
-wandb.watch(classifier_model)
-
-optimizer = optim.Adam(classifier_model.parameters(), weight_decay=1e-4, lr=1e-4)
-
-criterion = nn.CrossEntropyLoss()
-
-#train_loss_array = np.array([])
-#val_loss_array = np.array([])
-
-# In[16]:
-
-def l2_loss():
-    reg_coeff = 0.1
-    l2_reg = None
-    for p in classifier_model.parameters():
-        if l2_reg is None:
-            l2_reg = reg_coeff*p.norm(2)
-        else:
-            l2_reg = l2_reg +reg_coeff*p.norm(2)
-    
-    return l2_reg
-        
 def compute_class_accuracy(y_true, y_pred):
     cm = confusion_matrix(y_true, y_pred)
     cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
     return cm.diagonal()
 
 
-def train(epoch):
-#    global train_loss_array
+def train(epoch, model, optimizer, criterion, train_loader):
     
-    classifier_model.train()
+    model.train()
     train_loss = 0
     all_targets = np.array([])
     all_preds = np.array([])
     for batch_idx, (inputs, targets) in enumerate(train_loader):
         inputs, targets = inputs.to(device), targets.to(device)
         optimizer.zero_grad()
-        preds = classifier_model(inputs)
+        preds = model(inputs)
         
         loss = criterion(preds, targets)# + l2_loss()
         train_loss += loss.item()
@@ -244,19 +222,19 @@ def train(epoch):
     return avg_train_loss, acc, all_targets, all_preds
 
 
-def evaluation():
+def evaluation(model, criterion, val_loader):
     #global val_loss_array
     
     all_targets = np.array([])
     all_preds = np.array([])
     
-    classifier_model.eval()
+    model.eval()
     eval_loss = 0
     with torch.no_grad():
-        for inputs, targets in validation_loader:
+        for inputs, targets in val_loader:
 
             inputs, targets = inputs.to(device), targets.to(device)
-            preds = classifier_model(inputs)
+            preds = model(inputs)
 
             loss = criterion(preds, targets)
             eval_loss += loss.item()
@@ -275,11 +253,11 @@ def evaluation():
     
     return avg_val_loss, acc, all_targets, all_preds
         
-def test():
+def test(model, criterion, test_loader):
 
     #global test_loss_array
     
-    classifier_model.eval()
+    model.eval()
     test_loss = 0
     
     all_targets = np.array([])
@@ -288,7 +266,7 @@ def test():
         for inputs, targets in test_loader:
             inputs, targets = inputs.to(device), targets.to(device)
             
-            preds = classifier_model(inputs)
+            preds = model(inputs)
             
             test_loss += criterion(preds, targets).item()
 
@@ -307,33 +285,79 @@ def test():
     return test_loss, acc, all_targets, all_preds
 
 if __name__ == "__main__":
-    least_error=-1
-    for epoch in range(args.epochs):
 
-        train_loss, train_acc = train(epoch)
-        val_loss, val_acc, val_targets, val_preds = evaluation()
-        
-        if least_error==-1 or least_error>val_loss:
-            PATH = os.path.join('saved models', 'vae_classifier.pt')
-        
-            torch.save(classifier_model.state_dict(), PATH)
-            least_error = val_loss
+    test_loader = get_test_loader()
+    for fold in range(5):
+       
+        f = open(run_id+'_fold_'+str(fold)+'output.txt', 'w')
 
-        print('====> Epoch: {}  Train loss: {:.2f}   Train Accuracy: {:.2f}%   |  Validation loss: {:.2}   Validation Accuracy: {:.2f}%'.format(
-                  epoch, train_loss, train_acc*100, val_loss, val_acc*100))
+        wandb.init(project='hnultra', name=run_id+'_fold_'+str(fold)
+        wandb.update(config_dict)
 
-        wandb.log({'epoch':epoch, 'train_loss':train_loss, 'val_loss':val_loss})
-        
-    class_acc = compute_class_accuracy(val_targets, val_preds)
-    print("Validation class accuracy:")
-    print(class_acc)
+        train_loader, val_loader = get_train_and_val_loaders(fold)
 
-    test_loss, test_acc, test_targets, test_preds= test()
-    class_acc = compute_class_accuracy(test_targets, test_preds)
-    print("Test class accuracy:")
-    print(class_acc)
+        model = DeepClassifier(input_dim=args.input_dim, layers_dim=args.layers_dim).to(device)
+        wandb.watch(model)
+        optimizer = optim.Adam(model.parameters(), weight_decay=1e-4, lr=1e-4)
+        criterion = nn.CrossEntropyLoss()
 
-    print("Test Loss: {:.2f}   Test Accuracy: {:.2f}%".format(test_loss, test_acc*100))
+        least_error=-1
+
+        val_loss_array = np.array([])
+
+        for epoch in range(args.epochs):
+
+            train_loss, train_acc, train_targets, train_preds = train(epoch, model, optimizer, criterion, train_loader)
+            val_loss, val_acc, val_targets, val_preds = evaluation(model, criterion, val_loader)
+            
+            val_loss_array = np.append(val_loss_array, val_loss)
+
+            if least_error==-1 or least_error>val_loss:
+                PATH = os.path.join('saved models', run_id+'.pt')
+            
+                torch.save(model.state_dict(), PATH)
+                least_error = val_loss
+
+            print('====> Epoch: {}  Train loss: {:.2f}   Train Accuracy: {:.2f}%   |  Validation loss: {:.2}   Validation Accuracy: {:.2f}%'.format(
+                      epoch, train_loss, train_acc*100, val_loss, val_acc*100))
+
+            wandb.log({'train_loss':train_loss, 'val_loss':val_loss}, step=epoch)
+
+            precision_recall_fscore_support(train_targets, train_preds)
+            precision_recall_fscore_support(val_targets, val_preds)
+
+
+            if epoch >= 40 and val_loss_array[-1] >= min(val_loss_array[-20:-1]):
+                print('Early stopping')
+                break
+
+        #Load the model with the best performance
+        PATH = os.path.join('saved models', run_id+'.pt')
+        model.load_state_dict(torch.load(PATH))
+
+        val_loss, val_acc, val_targets, val_preds = evaluation(model, criterion, val_loader)
+        print('Best validation accuracy:', val_acc}
+        f.write('Best validation accuracy: {.2f} ... best validation loss: {.2f}\n'.format(val_acc, val_loss)
+
+        class_acc = compute_class_accuracy(val_targets, val_preds)
+        print("Validation class accuracy:")
+        print(class_acc)
+
+        f.write('validation class accuracy:' + str(class_acc) + '\n')
+
+        test_loss, test_acc, test_targets, test_preds= test(model, criterion, test_loader)
+        class_acc = compute_class_accuracy(test_targets, test_preds)
+        print("Test class accuracy:")
+        print(class_acc)
+
+        f.write('test class accuracy:' + str(class_acc) + '\n')
+
+
+        print("Test Loss: {:.2f}   Test Accuracy: {:.2f}%".format(test_loss, test_acc*100))
+
+        f.write("Test Loss: {:.2f}   Test Accuracy: {:.2f}%\n".format(test_loss, test_acc*100))
+
+        f.close()
 
 
     wandb.save(run_id+'.h5')
